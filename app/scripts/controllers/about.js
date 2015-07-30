3829cd03d530441133aaed80c5f8d24c3f21cbcd
Matthias Jacob <dev@matthiasjacob.net>
2015-07-31 00:13:33 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootstrapExampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularBootstrapExampleApp
+ */
+angular.module('angularBootstrapExampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBootstrapExampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBootstrapExampleApp
+ */
+angular.module('angularBootstrapExampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
